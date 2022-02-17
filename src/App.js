45900ba4784e4c0d45f17e453c11612f12022de6
lukaszkus <kusiu@vp.pl>
2022-02-17 11:25:51 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,68 @@
+import "./style.css";
+import Intro from "./components/Intro";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  return (
+    <>
+      <Intro />
+      <div class="container">
+        <div class="box box-1">
+          <img class="photo" src="./images/fon.png" alt="" />
+          <div class="overlay">
+            <div class="text">
+              <h2>Hello World</h2>
+              <p>Jakiś opisik..</p>
+            </div>
+          </div>
+        </div>
+        <div class="box box-2">
+          <img class="photo" src="./images/55.png" alt="" />
+          <div class="overlay">
+            <div class="text">
+              <h2>Hello World</h2>
+              <p>Jakiś opisik..</p>
+            </div>
+          </div>
+        </div>
+        <div class="box box-3">
+          <img class="photo" src="./images/11.png" alt="" />
+          <div class="overlay">
+            <div class="text">
+              <h2>Hello World</h2>
+              <p>Jakiś opisik..</p>
+            </div>
+          </div>
+        </div>
+        <div class="box box-4">
+          <img class="photo" src="./images/22.png" alt="" />
+          <div class="overlay">
+            <div class="text">
+              <h2>Hello World</h2>
+              <p>Jakiś opisik..</p>
+            </div>
+          </div>
+        </div>
+        <div class="box box-5">
+          <img class="photo" src="./images/33.png" alt="" />
+          <div class="overlay">
+            <div class="text">
+              <h2>Hello World</h2>
+              <p>Jakiś opisik..</p>
+            </div>
+          </div>
+        </div>
+        <div class="box box-6">
+          <img class="photo" src="./images/44.png" alt="" />
+          <div class="overlay">
+            <div class="text">
+              <h2>Hello World</h2>
+              <p>Jakiś opisik..</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
