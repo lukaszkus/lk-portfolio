44ea8ffb072d3ee0442f96597596d4b343005172
lukaszkus <kusiu@vp.pl>
2022-02-18 14:36:06 +0100

    cta added

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,24 +18,28 @@ const HeaderContainer = styled.header`
   align-items: center;
 `;
 
+const TitleContainer = styled.div``;
+
 const HeaderTitle = styled.h1`
-  font-size: 3rem;
+  font-size: 3.5rem;
   background-image: linear-gradient(to right, #6a11cb 0%, #2575fc 100%);
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+  position: relative;
 
   @media ${breakpoint.device.tablet} {
-    font-size: 4rem;
+    font-size: 4.5rem;
   }
 
   @media ${breakpoint.device.desktop} {
-    font-size: 6rem;
+    font-size: 6.5rem;
   }
 `;
 
-const Subtitle = styled.div`
+const HeaderSubtitle = styled.div`
   height: 30px;
   overflow: hidden;
+  margin-bottom: 50px;
 
   @media ${breakpoint.device.tablet} {
     height: 55px;
@@ -68,16 +72,15 @@ const moveDesktop = keyframes`
 
 const SubtitleWrapper = styled.div`
   height: 100%;
-  animation: ${move} 6s ease-in-out infinite alternate;
+  animation: ${move} 5s ease-in-out infinite alternate;
+  animation-delay: 1.5s;
 
   @media ${breakpoint.device.tablet} {
-    height: 100%;
-    animation: ${moveTablet} 6s ease-in-out infinite alternate;
+    animation-name: ${moveTablet};
   }
 
   @media ${breakpoint.device.desktop} {
-    height: 100%;
-    animation: ${moveDesktop} 6s ease-in-out infinite alternate;
+    animation-name: ${moveDesktop};
   }
 `;
 
@@ -88,17 +91,24 @@ const SubtitleItem = styled.div`
   font-weight: 600;
   display: flex;
   align-items: center;
-  background-image: linear-gradient(
-    to right,
-    #b8cbb8 0%,
-    #b8cbb8 0%,
-    #b465da 0%,
-    #cf6cc9 33%,
-    #ee609c 66%,
-    #ee609c 100%
-  );
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
+
+  &:first-child {
+    background-image: linear-gradient(
+      to right,
+      #b8cbb8 0%,
+      #b8cbb8 0%,
+      #b465da 0%,
+      #cf6cc9 33%,
+      #ee609c 66%,
+      #ee609c 100%
+    );
+  }
+
+  &:last-child {
+    background-image: linear-gradient(to right, #209cff 0%, #68e0cf 100%);
+  }
 
   @media ${breakpoint.device.tablet} {
     font-size: 3rem;
@@ -111,18 +121,44 @@ const SubtitleItem = styled.div`
   }
 `;
 
+const HeaderCTA = styled.a`
+  color: rgba(0, 0, 0, 0.3);
+  font-family: "Poppins", sans-serif;
+  font-size: 0.75rem;
+  font-weight: 300;
+  padding: 0.9rem 2rem;
+  border: 1px solid rgba(0, 0, 0, 0.3);
+  border-radius: 2.25rem;
+
+  &:hover {
+    transition: all 0.5s ease-in-out;
+    color: rgba(0, 0, 0, 0.7);
+    border: 1px solid rgba(0, 0, 0, 0.7);
+    cursor: pointer;
+  }
+
+  @media ${breakpoint.device.tablet} {
+    font-size: 1rem;
+  }
+
+  @media ${breakpoint.device.desktop} {
+    font-size: 1.25rem;
+  }
+`;
+
 function Header() {
   return (
     <HeaderContainer>
-      <HeaderTitle>
-        Hi, I'm Kusiu.
-        <Subtitle>
+      <TitleContainer>
+        <HeaderTitle>Hi, I'm Kusiu.</HeaderTitle>
+        <HeaderSubtitle>
           <SubtitleWrapper>
             <SubtitleItem>FRONT-END DEVELOPER</SubtitleItem>
             <SubtitleItem>GRAPHIC DESIGNER</SubtitleItem>
           </SubtitleWrapper>
-        </Subtitle>
-      </HeaderTitle>
+        </HeaderSubtitle>
+        <HeaderCTA>Get in touch</HeaderCTA>
+      </TitleContainer>
     </HeaderContainer>
   );
 }
