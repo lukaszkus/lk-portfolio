275e6745497f63bfeec90491d799213cac16bfea
lukaszkus <kusiu@vp.pl>
2023-03-23 23:57:09 +0100

    removed context from Topbar

--- src/components/Topbar/Topbar.jsx
+++ src/components/Topbar/Topbar.jsx
@@ -1,17 +1,13 @@
-import { useContext } from "react";
-import Context from "../../context/context";
 import { Logo, MenuBtn, Menu } from "../";
 
 import { Wrapper } from "./Topbar.style";
 
 const Topbar = () => {
-  const { isMenuOpen } = useContext(Context);
-
   return (
     <Wrapper>
       <Logo />
       <MenuBtn />
-      {isMenuOpen && <Menu />}
+      <Menu />
     </Wrapper>
   );
 };
